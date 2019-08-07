0df78ab4b23b7d35ca1ee0bab214797bc2b9361a
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-08-07 15:53:41 -0500

    :construction: Adds Validation

--- onboarding/src/components/Form.js
+++ onboarding/src/components/Form.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const LoginForm = () => {
+const LoginForm = (errors, touched) => {
 
   return (
     <div className="loginForm">
       <Form>
-        <Field type="text" name="name" placeholder="Name" />
-        <Field type="email" name="email" placeholder="Email" />
-        <Field type="password" name="password" placeholder="Password" />
+        <div>
+          {touched.name && errors.name && <p>{errors.name}</p>}
+          <Field type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+          {touched.email && errors.email && <p>{errors.email}</p>}
+          <Field type="email" name="email" placeholder="Email" />
+        </div>
+        <div>
+          {touched.password && errors.password && <p>{errors.password}</p>}
+          <Field type="password" name="password" placeholder="Password" />
+        </div>
         <label >
-          Confirm you have read our <a>terms of service</a>:
           <Field type="checkbox" name="tos" />
+          Acknowledge our <a href="https://en.wikipedia.org/wiki/Terms_of_service">terms of service</a>
         </label>
-        <button>Submit</button>
+        <div>
+          <button>Submit</button>
+        </div>
       </Form>
     </div>
   )
@@ -29,6 +41,21 @@ const FormikLoginForm = withFormik({
       tos: tos || false
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .trim("looks like there's a whitespace before or after your name.")
+      .required("Please type in your name here."),
+    email: Yup.string()
+      .email("Must be a valid email format (username@mail.com)")
+      .required("Please include an email address"),
+    password: Yup.string()
+      .min(8, "Password must be 8 charaters or longer")
+      .required("Please include your password"),
+    tos: Yup.boolean()
+      .default(false, "Please review our Terms of Service and check the box to indicate that you have.")
+      .required("Please review our Terms of Service and check the box to indicate that you have.")
+  }),
 
   handleSubmit(values) {
     console.log(values);
